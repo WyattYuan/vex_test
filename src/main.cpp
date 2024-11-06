e246d6eedb764450fd2baa93c941330178ed82ea
WyattYuan <Wyatt049@outlook.com>
2024-11-06 23:04:59 +0800

    我删掉了那些注释，并且使用了Microsoft格式

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-// 袁梓锋 能够成功使用git
-//果宝特攻
 #include "main.h"
 #define LEFT_FRONT_WHEELS_PORT 1
 #define RIGHT_FRONT_WHEELS_PORT 10
@@ -20,14 +18,18 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+void on_center_button()
+{
+    static bool pressed = false;
+    pressed = !pressed;
+    if (pressed)
+    {
+        pros::lcd::set_text(2, "I was pressed!");
+    }
+    else
+    {
+        pros::lcd::clear_line(2);
+    }
 }
 
 /**
@@ -36,11 +38,12 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "We are GUOBAOTEGONG Team!");
+void initialize()
+{
+    pros::lcd::initialize();
+    pros::lcd::set_text(1, "We are GUOBAOTEGONG Team!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+    pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -48,7 +51,9 @@ void initialize() {
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void disabled()
+{
+}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -59,7 +64,9 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize()
+{
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -72,13 +79,14 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {
-	pros::Motor left_wheels (LEFT_FRONT_WHEELS_PORT);
-    pros::Motor right_wheels (RIGHT_FRONT_WHEELS_PORT);
+void autonomous()
+{
+    pros::Motor left_wheels(LEFT_FRONT_WHEELS_PORT);
+    pros::Motor right_wheels(RIGHT_FRONT_WHEELS_PORT);
     right_wheels.set_reversed(false); // This reverses the motor
 
-  	right_wheels.move_relative(1000, MOTOR_MAX_SPEED);
-  	left_wheels.move_relative(1000, MOTOR_MAX_SPEED);
+    right_wheels.move_relative(1000, MOTOR_MAX_SPEED);
+    left_wheels.move_relative(1000, MOTOR_MAX_SPEED);
 }
 
 /**
@@ -95,33 +103,34 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 
-
-void opcontrol() {
+void opcontrol()
+{
     // 初始化左前轮电机
-    pros::Motor left_front_wheel (LEFT_FRONT_WHEELS_PORT);
+    pros::Motor left_front_wheel(LEFT_FRONT_WHEELS_PORT);
     // 初始化右前轮电机，并设置为反转
-    pros::Motor right_front_wheel (RIGHT_FRONT_WHEELS_PORT);
+    pros::Motor right_front_wheel(RIGHT_FRONT_WHEELS_PORT);
     right_front_wheel.set_reversed(true);
     // 初始化左后轮电机
-    pros::Motor left_rear_wheel (LEFT_REAR_WHEELS_PORT);
+    pros::Motor left_rear_wheel(LEFT_REAR_WHEELS_PORT);
     // 初始化右后轮电机，并设置为反转
-    pros::Motor right_rear_wheel (RIGHT_REAR_WHEELS_PORT);
+    pros::Motor right_rear_wheel(RIGHT_REAR_WHEELS_PORT);
     right_rear_wheel.set_reversed(true);
     // 初始化主控制器
-    pros::Controller master (CONTROLLER_MASTER);
+    pros::Controller master(CONTROLLER_MASTER);
 
-    while (true) {
+    while (true)
+    {
         // 获取左摇杆的Y轴值，用于控制前进和后退
         int power = master.get_analog(ANALOG_LEFT_Y);
         // 获取右摇杆的X轴值，用于控制转向
         int turn = master.get_analog(ANALOG_RIGHT_X);
         int left = 2 * (power + turn);
-        int right = 2 * (power - turn);  
+        int right = 2 * (power - turn);
         left_front_wheel.move(left);
         right_front_wheel.move(right);
         left_rear_wheel.move(left);
         right_rear_wheel.move(right);
-        
+
         pros::delay(2);
     }
 }
